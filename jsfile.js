48f16d934026311ac2929e7eb7db3aaccd9651df
MMatveieva <matveieva.mariya@gmail.com>
2017-02-04 20:55:23 +0200

    50/50 working js

--- jsfile.js
+++ jsfile.js
@@ -2,9 +2,22 @@
  * Created by Mariya on 01.02.2017.
  */
 
+console.log("Hello");
+
 $(function () {
+    // variables from column one
     var $LIST = $('.shop-list');
-    var $ITEM_TEMPLATE = $('.ROW-TEMPLATE').html();
+    var $ITEM_TEMPLATE = $('.row-template').html();
+    var $addButton = $(".add-button");
+    var $input = $(".add-item");
+
+    //variables from column two
+    var $NOT_BOUGHT = $('.elements-not-bought');
+    var $BOUGHT = $('.bought');
+
+    // console.log("Template:" + $ITEM_TEMPLATE);
+    console.log("add-button:", $addButton);
+    console.log("input:", $input);
 
     function addItem(title) {
         var $node = $($ITEM_TEMPLATE);
@@ -12,34 +25,38 @@ $(function () {
         var $quantity_label = $node.find(".count");
         $quantity_label.text(quantity);
 
-        $node.find(".item").text(title);
+        $node.find(".name").text(title);
+        console.log("Name", $node.find(".name").text(title));
+        console.log("Quantity-label", quantity);
 
         $node.find(".delete-button").click(function () {
             $node.remove();
         });
 
-        $node.find(".minus-button").click(function () {
+        var $minusButton = $(".minus-button");
+        $node.find($minusButton).click(function () {
+            console.log("Quantity", quantity);
             if (quantity > 1) {
+
                 quantity--;
                 $quantity_label.text(quantity);
             }
+
         });
 
         $node.find(".plus-button").click(function () {
             quantity++;
+            if (quantity > 1)
+                $minusButton.removeClass("disabled");
             $quantity_label.text(quantity);
         });
 
         $LIST.append($node); //Add to the end of the list
     }
 
-    addItem("orange 1");
-
-    var $addButton = $(".plus-button");
-    var $input = $(".new-item");
-
     function addNewItem() {
         var item_name = $input.val();
+        console.log(item_name);
         if (item_name.trim()) {
             addItem(item_name);
         }
@@ -47,7 +64,11 @@ $(function () {
         $input.focus();
     }
 
-    $input.attr("placeholder", "Введіть назву товару");
+    //When you click on button "ADD", new item appears on the list
+    $addButton.click(addNewItem);
+
+
+    $input.attr("placeholder", "Назву товару");
 
 
     function updateNode(node, fn) {
